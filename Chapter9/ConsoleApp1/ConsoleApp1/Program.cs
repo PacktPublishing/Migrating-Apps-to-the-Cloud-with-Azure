ac2c965fdff74fe656ea6bbeffd0fdf1f3b181b9
Sander Rossel <sander.rossel@outlook.com>
2018-10-26 21:18:20 +0200

    Code for chapter 9
    A little console app to put something on a queue and a Function application with some triggered functions.

---
+++ Chapter9/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.ServiceBus;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            string text = "This is some text.";
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+            QueueClient client = new QueueClient("[your connectionstring]", "myqueue");
+            var msg = new Message(bytes);
+            msg.Label = "Some label";
+            client.SendAsync(msg).Wait();
+            client.CloseAsync().Wait();
+        }
+    }
+}
